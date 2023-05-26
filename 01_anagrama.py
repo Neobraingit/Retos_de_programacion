309e9964e0d204b8e8b2d2bbcba48aac1d881ba7
Neobraingit <Maervca2@icloud.com>
2023-05-26 15:23:58 +0200

    Anagrama

---
+++ 01_anagrama.py
@@ -0,0 +1,14 @@
+'''/*
+ * Escribe una función que reciba dos palabras (String) y retorne
+ * verdadero o falso (Bool) según sean o no anagramas.
+ * - Un Anagrama consiste en formar una palabra reordenando TODAS
+ *   las letras de otra palabra inicial.
+ * - NO hace falta comprobar que ambas palabras existan.
+ * - Dos palabras exactamente iguales no son anagrama.
+ */'''
+
+palabra = input('Introduce una palabra: ')
+if palabra == palabra[::-1]:
+    print ('Es un anagrama')
+else:
+    print ('No es una anagrama')
