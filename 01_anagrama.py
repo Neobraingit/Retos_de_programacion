f3969f6c27ba3bdc80819e7d8204f03b5702eec3
Neobraingit <Maervca2@icloud.com>
2023-05-26 15:25:30 +0200

    anagrama

--- 01_anagrama.py
+++ 01_anagrama.py
@@ -7,8 +7,26 @@
  * - Dos palabras exactamente iguales no son anagrama.
  */'''
 
-palabra = input('Introduce una palabra: ')
-if palabra == palabra[::-1]:
-    print ('Es un anagrama')
+def son_anagramas(palabra1, palabra2):
+    if len(palabra1) != len(palabra2):
+        return False
+
+    caracteres1 = list(palabra1)
+    caracteres2 = list(palabra2)
+
+    for caracter in caracteres1:
+        if caracter in caracteres2:
+            caracteres2.remove(caracter)
+        else:
+            return False
+
+    return len(caracteres2) == 0
+
+# Ejemplo de uso:
+palabra1 = input("Ingresa la primera palabra: ")
+palabra2 = input("Ingresa la segunda palabra: ")
+
+if son_anagramas(palabra1, palabra2):
+    print("Las palabras son anagramas.")
 else:
-    print ('No es una anagrama')
+    print("Las palabras no son anagramas.")
