db60c60a6a07f8ac7be58e38b2d4127344f6c65d
Neobraingit <Maervca2@icloud.com>
2023-05-28 09:13:09 +0200

    100 ejercicios

---
+++ 06_invirtiendo_cadenas.py
@@ -0,0 +1,8 @@
+'''/*
+ * Crea un programa que invierta el orden de una cadena de texto
+ * sin usar funciones propias del lenguaje que lo hagan de forma automática.
+ * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+ */'''
+cadena = input('Invierte una cadena')
+invertida = cadena[::-1]
+print (invertida)
