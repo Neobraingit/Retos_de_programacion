63bfe7bbdd1b95f9e4f22e39462aa37dc8e9b362
Neobraingit <Maervca2@icloud.com>
2023-06-02 11:49:09 +0200

    Retos de programación

--- 19_conversor_tiempo.py
+++ 19_conversor_tiempo.py
@@ -3,4 +3,16 @@
  * y retorne su resultado en milisegundos.
  */'''
  
- 
+def convertir_a_milisegundos(dias, horas, minutos, segundos):
+        total_segundos = segundos + minutos * 60 + horas * 3600 + dias * 86400
+        milisegundos = total_segundos * 1000
+        return milisegundos
+
+# Ejemplo de uso
+d = 2  # días
+h = 4  # horas
+m = 30  # minutos
+s = 45  # segundos
+
+resultado = convertir_a_milisegundos(d, h, m, s)
+print(resultado)  # Output: 185085000
